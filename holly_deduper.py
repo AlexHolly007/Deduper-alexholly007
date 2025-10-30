9bb5b46796143d316db03d088f13a5424490ea53
Alex Holly <alho@uoregon.edu>
2025-10-29 19:35:12 -0700

    test files run good, moving to talapas

--- holly_deduper.py
+++ holly_deduper.py
@@ -15,67 +15,67 @@ def main(sam_file, outfile, umi_file):
     last_chrom = 0
     
     with open(outfile, 'w') as fo:
+        with open(sam_file, 'r') as fi:
+            for line in fi:
+                if line[0] == '@':
+                    fo.write(line)
+                    continue
 
-        for line in sam_file:
-            if line[0] == '@':
-                fo.write(line)
-                continue
+                fields = line.split()
 
-            fields = line.split()
+                #umi
+                umi = fields[0][-8:]
+                if umi not in valid_umis:
+                    continue
 
-            #umi
-            umi = fields[0][-8:]
-            if umi not in valid_umis:
-                continue
+                #strand
+                strand = 1 if int(fields[1]) & 16 else 0
 
-            #strand
-            strand = 1 if int(fields[1]) & 16 else 0
+                #chrom
+                chrom = fields[2].strip()
+                if chrom != last_chrom:
+                    dup_dict = {}
+                    last_chrom = chrom
 
-            #chrom
-            chrom = fields[2].strip()
-            if chrom != last_chrom:
-                dup_dict = {}
-                last_chrom = chrom
+                #R prime start location
+                loc_5 = five_prime_start_finder(fields[5].strip(), int(fields[3].strip()), strand)
 
-            #R prime start location
-            loc_5 = five_prime_start_finder(fields[5].strip(), int(fields[3].strip()), strand)
+                #DUP CHECKING with memory contrainsts-------------
 
-            #DUP CHECKING with memory contrainsts-------------
-
-            #get last 4 digits of 5 prim location
-            last_four = str(loc_5 % 10000).zfill(4)
-            prefix_pos = 0 #will be 0 when position isnt above 10000 (important for dictionary)
-            if len(str(loc_5)) > 4:
-                #get prefix numbers before those last 4 digits
-                prefix_pos = str(loc_5)[:-4]
-            
-            if last_four in dup_dict:
-                dict_entry = dup_dict[last_four]
+                #get last 4 digits of 5 prim location
+                last_four = str(loc_5 % 10000).zfill(4)
+                prefix_pos = 0 #will be 0 when position isnt above 10000 (important for dictionary)
+                if len(str(loc_5)) > 4:
+                    #get prefix numbers before those last 4 digits
+                    prefix_pos = str(loc_5)[:-4]
                 
-                if prefix_pos == dict_entry[0]:
-                    if [umi,strand] in dict_entry[1]:
-                        continue
+                if last_four in dup_dict:
+                    dict_entry = dup_dict[last_four]
                     
-                    #if were at the same position, and its not a dup with umi and strand, write to file and add to dict
-                    else:
-                        dup_dict[last_four][1].append([umi,strand])
-                        #WRITEEEEEEEE
-                        fo.write(line)
-                    
+                    if prefix_pos == dict_entry[0]:
+                        if [umi,strand] in dict_entry[1]:
+                            continue
+                        
+                        #if were at the same position, and its not a dup with umi and strand, write to file and add to dict
+                        else:
+                            dup_dict[last_four][1].append([umi,strand])
+                            #WRITEEEEEEEE
+                            fo.write(line)
+                        
                 
 
-            else:
-                #add to dict and write to file
-                dup_dict[last_four] = [prefix_pos, [[umi, strand]]]
-                #WRITEEEEEEEE
-                fo.write(line)
+                else:
+                    #add to dict and write to file
+                    dup_dict[last_four] = [prefix_pos, [[umi, strand]]]
+                    #WRITEEEEEEEE
+                    fo.write(line)
 
 
-            #MEMORY ISSUE SOLUTION::
-            ###     have a dictionary/somthing with indexes 000-9999. its value will be a list: [prefix position, [list of [umi,strand] entries ] ]
-            ###     so if entry is 5 prime location 19101, entry at position 9101 is [1, [list of UMI, Strand with that pos] ]
-            ###     if when you get to dictionary entry ## (like 01), and the first value in the list isnt what the location is, remove the whole entry for and restart it
-            ###     now, the dictionary will clean itself to always stay low memory, but not waste to much time remaking itself. capable for any file size
+                #MEMORY ISSUE SOLUTION::
+                ###     have a dictionary/somthing with indexes 000-9999. its value will be a list: [prefix position, [list of [umi,strand] entries ] ]
+                ###     so if entry is 5 prime location 19101, entry at position 9101 is [1, [list of UMI, Strand with that pos] ]
+                ###     if when you get to dictionary entry ## (like 01), and the first value in the list isnt what the location is, remove the whole entry for and restart it
+                ###     now, the dictionary will clean itself to always stay low memory, but not waste to much time remaking itself. capable for any file size
             
 
     return
@@ -124,7 +124,8 @@ def five_prime_start_finder(cigar_str : str, given_position: int, strand: int) -
                     pos += int(new_cig_str.groups()[0])
             
                 #remove the first cigar sequence, so we can look at next
-                new_cig_str = cigar_str[len(new_cig_str.groups()[0])+1:]
+                cigar_str = cigar_str[len(new_cig_str.groups()[0])+1:]
+                new_cig_str = re.search('([0-9]+)([A-Z])', cigar_str)
 
             #cigar str over
             else:
@@ -148,8 +149,9 @@ def five_prime_start_finder(cigar_str : str, given_position: int, strand: int) -
 def build_umi_set(umi_file: str) -> set:
     umi_set = set()
 
-    for line in umi_file:
-        umi_set.add(line.strip())
+    with open(umi_file, 'r') as fi:
+        for line in fi:
+            umi_set.add(line.strip())
 
     return umi_set
 
@@ -167,7 +169,10 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    #main(args.file, args.outfile, args.umi)
+    main(args.file, args.outfile, args.umi)
     five_prime_start_finder('1S',100, 0)
     five_prime_start_finder('111S',100, 0)
     five_prime_start_finder('999M',100, 0)
+
+    #test file run
+    #python3 holly_deduper.py -f Testing_Dir/input.sam -o Testing_Dir/test_output.sam -u STL96.txt
